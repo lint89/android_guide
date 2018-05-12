f16a7d0e6162e2356e43c90a677aa6d8a2cd5b65
lint <linsir345@gmail.com>
2018-05-12 22:13:03 +0800

    ch02: add class Question, add next button.

---
+++ GeoQuiz/app/src/main/java/com/diapearl/lint/geoquiz/Question.java
@@ -0,0 +1,27 @@
+package com.diapearl.lint.geoquiz;
+
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+}
