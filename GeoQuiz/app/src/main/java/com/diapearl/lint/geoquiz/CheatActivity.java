12024753f5ddf764d1903fd41f280dae1b78184e
lint <linsir345@gmail.com>
2018-05-16 22:28:06 +0800

    [GeoQuiz] add anima effect in cheatActivity

--- GeoQuiz/app/src/main/java/com/diapearl/lint/geoquiz/CheatActivity.java
+++ GeoQuiz/app/src/main/java/com/diapearl/lint/geoquiz/CheatActivity.java
@@ -1,10 +1,14 @@
 package com.diapearl.lint.geoquiz;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Animatable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -39,6 +43,30 @@ public class CheatActivity extends AppCompatActivity {
         return result.getBooleanExtra(EXTRA_ANSWER_SHOWN, false);
     }
 
+    private void showAnswerButtonAnima() {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            int cx = mShowAnswerButton.getWidth() / 2;
+            int cy = mShowAnswerButton.getHeight() / 2;
+
+            float radius = mShowAnswerButton.getWidth();
+
+            Animator anim = ViewAnimationUtils.createCircularReveal(
+                    mShowAnswerButton, cx, cy, radius, 0
+            );
+
+            anim.addListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    super.onAnimationEnd(animation);
+                    mShowAnswerButton.setVisibility(View.INVISIBLE);
+                }
+            });
+            anim.start();
+        } else {
+            mShowAnswerButton.setVisibility(View.INVISIBLE);
+        }
+    }
+
     private void setAnswerTextView() {
         if (mAnswerIsTrue) {
             mAnswerTextView.setText(R.string.true_button);
@@ -47,6 +75,7 @@ public class CheatActivity extends AppCompatActivity {
         }
 
         setAnswerResult(true);
+        showAnswerButtonAnima();
     }
 
     @Override
