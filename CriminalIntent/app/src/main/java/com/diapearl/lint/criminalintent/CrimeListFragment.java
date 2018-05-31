18a6eb45c5a78871daeff3b9cf72cceaa4fe32a0
lint <linsir345@gmail.com>
2018-05-31 23:25:05 +0800

    [CriminalIntent] put Extra info in simple way

--- CriminalIntent/app/src/main/java/com/diapearl/lint/criminalintent/CrimeListFragment.java
+++ CriminalIntent/app/src/main/java/com/diapearl/lint/criminalintent/CrimeListFragment.java
@@ -75,7 +75,7 @@ class CrimeListFragment extends Fragment {
 
         @Override
         public void onClick(View v) {
-            Intent intent = new Intent(getActivity(), CrimeActivity.class);
+            Intent intent = CrimeActivity.newIntent(getActivity(), mCrime.getId());
             startActivity(intent);
         }
     }
