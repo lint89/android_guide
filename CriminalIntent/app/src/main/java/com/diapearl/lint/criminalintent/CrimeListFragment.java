07c26bca7ddbd80f0a6950a8bf2b0fab2332f4db
lint <linsir345@gmail.com>
2018-06-10 22:28:26 +0800

    [CriminalIntent] add sqllite support

--- CriminalIntent/app/src/main/java/com/diapearl/lint/criminalintent/CrimeListFragment.java
+++ CriminalIntent/app/src/main/java/com/diapearl/lint/criminalintent/CrimeListFragment.java
@@ -27,6 +27,7 @@ import java.util.List;
 class CrimeListFragment extends Fragment {
     private RecyclerView mCrimeRecyclerView;
     private CrimeAdapter mAdapter;
+    private boolean mSubtitleVisible;
     private int mCurrrentPosistion = 0;
 
     @Override
@@ -63,6 +64,7 @@ class CrimeListFragment extends Fragment {
             mAdapter = new CrimeAdapter(crimes);
             mCrimeRecyclerView.setAdapter(mAdapter);
         } else {
+            mAdapter.setCrimes(crimes);
             mAdapter.notifyItemChanged(mCurrrentPosistion);
         }
     }
@@ -71,6 +73,13 @@ class CrimeListFragment extends Fragment {
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
         inflater.inflate(R.menu.fragment_crime_list, menu);
+
+        MenuItem subtitleItem = menu.findItem(R.id.show_subtitle);
+        if (mSubtitleVisible) {
+            subtitleItem.setTitle(R.string.hide_subtitle);
+        } else {
+            subtitleItem.setTitle(R.string.show_subtitle);
+        }
     }
 
     @Override
@@ -85,9 +94,11 @@ class CrimeListFragment extends Fragment {
                 return true;
 
             case R.id.show_subtitle:
+                mSubtitleVisible = ! mSubtitleVisible;
+                getActivity().invalidateOptionsMenu();
                 updateSubtitle();
                 return true;
-                
+
             default:
                 return super.onOptionsItemSelected(item);
         }
@@ -97,6 +108,10 @@ class CrimeListFragment extends Fragment {
         CrimeLab crimeLab = CrimeLab.get(getActivity());
         int crimeCount = crimeLab.getCrimes().size();
         String subtitle = getString(R.string.subtitle_format, crimeCount);
+
+        if (!mSubtitleVisible) {
+            subtitle = null;
+        }
 
         AppCompatActivity activity = (AppCompatActivity) getActivity();
         activity.getSupportActionBar().setSubtitle(subtitle);
@@ -161,6 +176,10 @@ class CrimeListFragment extends Fragment {
         @Override
         public int getItemCount() {
             return mCrimes.size();
+        }
+
+        public void setCrimes(List<Crime> crimes) {
+            mCrimes = crimes;
         }
 
     }
